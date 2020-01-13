4a273f29884bfe204e6fab1e51a87420c0de8239
DhruvalInexture <Dhruval.inexture@gmail.com>
2020-01-13 13:13:22 +0530

    apply transform on navbar and footer

--- js/home.js
+++ js/home.js
@@ -1,20 +1,91 @@
 function onHover(obj){
-    $("h4:not(."+obj+" h4)").css( {"opacity":"0","z-index":"5","transition":"z-index 0.4s"} );
-    $("span:not(."+obj+" span)").css( {"opacity":"0","z-index":"5","transition":"z-index 0.4s"} );
-    $("."+obj+" h4").css( {"color":"#ff6c3f","transition":"color 1s"} );
-    $("."+obj+" span").css( {"color":"#ff6c3f","transition":"color 1s"} );
-    $("."+obj+" img").css({"z-index":"2","opacity":"1","transition":"all 0.5s"});
-    $("."+obj+" video").css({"z-index":"2","opacity":"1","transition":"all 0.5s"});
+    $("h4:not(."+obj+" h4)").css({
+        "opacity":"0",
+        "z-index":"5",
+        "transition":"z-index 0.4s"
+    });
+
+    $("span:not(."+obj+" span)").css({
+        "opacity":"0",
+        "z-index":"5",
+        "transition":"z-index 0.4s"
+    });
+
+    $("."+obj+" h4").css({
+        "color":"#ff6c3f",
+        "transition":"color 1s"
+    });
+
+    $("."+obj+" span").css({
+        "color":"#ff6c3f",
+        "transition":"color 1s"
+    });
+
+    $("."+obj+" img").css({
+        "z-index":"2",
+        "opacity":"1",
+        "transition":"all 0.5s"
+    });
+
+    $("."+obj+" video").css({
+        "z-index":"2",
+        "opacity":"1",
+        "transition":"all 0.5s"
+    });
+
     document.getElementById("fisher").load();
+
+    $("#footer").css({
+        "transform":"translateY(50%)",
+        "opacity":"0",
+        "transition":"all 0.5s"
+    });
+
+    $("nav").css({
+        "transform":"translateY(-50%)",
+        "opacity":"0",
+        "transition":"all 0.5s"
+    });
 }
 
 function outHover(obj){
-    $("h4:not(."+obj+" h4)").css( "opacity","1" );
-    $("span:not(."+obj+" span)").css( "opacity","1" );
-    $("."+obj+" h4").css( {"color":"black","transition":"color 1s"} );
-    $("."+obj+" span").css( {"color":"black","transition":"color 1s"} );
-    $("."+obj+" img").css({"z-index":"-1","opacity":"0","transition":"all 0.9s"});
-    $("."+obj+" video").css({"z-index":"-1","opacity":"0","transition":"all 0.9s"});      
+    $("h4:not(."+obj+" h4)").css("opacity","1");
+
+    $("span:not(."+obj+" span)").css("opacity","1");
+
+    $("."+obj+" h4").css({
+        "color":"black",
+        "transition":"color 1s"
+    });
+
+    $("."+obj+" span").css({
+        "color":"black",
+        "transition":"color 1s"
+    });
+
+    $("."+obj+" img").css({
+        "z-index":"-1",
+        "opacity":"0",
+        "transition":"all 0.9s"
+    });
+
+    $("."+obj+" video").css({
+        "z-index":"-1",
+        "opacity":"0",
+        "transition":"all 0.9s"
+    });      
+
+    $("#footer").css({
+        "transform":"translateY(0)",
+        "opacity":"1",
+        "transition":"all 0.5s"
+    });
+
+    $("nav").css({
+        "transform":"translateY(0)",
+        "opacity":"1",
+        "transition":"all 0.5s"
+    })
 }
 
 $(window).on('load', function(){
