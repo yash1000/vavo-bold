685a899eb2fcbb261ba852cd3999629672c8ed62
abhishek.satasiya <abhisheks.inexture@gmail.com>
2020-01-20 12:35:27 +0530

    journal pages

--- js/bettany-hamilton.js
+++ js/bettany-hamilton.js
@@ -5,3 +5,22 @@ $('.main-carousel').flickity({
   });
 
   AOS.init();
+
+  $(window).on('load', function(){
+    $(".hero-animation").css({
+        "transform":"rotate(0)",
+        "transformOrigin":"top right",
+        "transition":"all 1s ease",
+        "opacity":"1"
+    })
+
+    $(".hero-animation1").css({
+      "transform":"rotate(0)",
+
+      "transformOrigin":"top",
+      "transition":"all 1s ease",
+      "opacity":"1"
+  })
+
+
+    });
